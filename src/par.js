239f93ddc6473657397b622104fdb4032d8fe101
FelixMo42-school <felixmoses@students.berkeley.net>
2020-02-11 14:26:16 -0800

    error messages

--- src/par.js
+++ src/par.js
@@ -1,17 +1,20 @@
-let tokens = [
-    "I",
-    "E",
-    "I",
-    "I",
-    "E",
-    "I"
-]
+const {red, gray, green, blue} = require("chalk")
 
-let file = "let a = (+ 1 1)"
+function Token(type, value="") {
+    return {type, value}
+}
+
+let tokens = [
+    Token("let", "let"),
+    Token("identifier", "a"),
+    Token("equal", "="),
+    Token("identifier", "abcd")
+]
 
 const types = {
     PATTERN: 0,
-    TOKEN: 1
+    TOKEN: 1,
+    OR: 2
 }
 
 const modes = {
@@ -20,15 +23,27 @@ const modes = {
     LOOP: 2
 }
 
+let possibilities = new Map()
+
+function addPossibility(index, possiblity) {
+    if ( !possibilities.has(index) ) {
+        possibilities.set(index, [])
+    }
+
+    possibilities.get(index).push(possiblity)
+}
+
 function Match(rule, index) {
     if (rule.type == types.TOKEN) {
-        if (rule.name == tokens[index]) {
+        if (index < tokens.length && rule.name == tokens[index].type) {
             return {
                 succses: true,
-                value: tokens[index],
+                value: tokens[index].value,
                 index: index + 1
             }
         } else {
+            addPossibility(index, rule.name)
+
             return {
                 succses: false,
                 index: index
@@ -39,7 +54,7 @@ function Match(rule, index) {
     if (rule.type == types.PATTERN) {
         let node = {}
 
-        for (let step of rule.steps) {
+        for (let step of rule.rule) {
             let ogIndex = index
 
             let match = Parse(step, index)
@@ -104,28 +119,100 @@ function Parse(rule, index=0) {
     }
 }
 
-console.log( Parse({
-    type: types.PATTERN,
-    mode: modes.LOOP,
-    steps: [
+///// SETUP //////
+
+let file = (() => {
+    let number = {
+        type: types.TOKEN,
+        mode: modes.NEXT,
+        name: "number"
+    }
+
+    let letStatment = []
+
+    let value = {
+        type: types.OR,
+        mode: modes.NEXT,
+
+        rules: [
+            letStatment,
+            number
+        ]
+    }
+
+    letStatment.join([
         {
             type: types.TOKEN,
             mode: modes.NEXT,
-            name: "I",
+            name: "let"
+        },
+        {
+            type: types.TOKEN,
+            mode: modes.NEXT,
+            name: "identifier",
 
             as: "name"
         },
         {
             type: types.TOKEN,
             mode: modes.NEXT,
-            name: "E"
+            name: "equal"
         },
         {
-            type: types.TOKEN,
-            mode: modes.LOOP,
-            name: "I",
+            ...number,
 
-            as: "steps"
+            as: "value"
         }
-    ]
-}, 0).value )
+    ])
+
+    console.log(letStatment)
+
+    let file = {
+        type: types.PATTERN,
+        mode: modes.LOOP,
+        rule: letStatment
+    }
+
+    return file
+})()
+
+///// DO IT /////
+
+;(() => {
+
+    let {value, index} = Parse(file, 0)
+
+    if (index < tokens.length) {
+        possibilities.forEach( (values, index) => {
+            let got = index < tokens.length ? tokens[index].type : "EOF"
+
+            values.forEach((value) => {
+                let expected = value
+
+                console.log(
+                    `${red("ERROR:")} got ${green(got)}, expected a ${green(expected)} on token #${index + 1}`
+                )
+            })
+
+            console.log()
+
+            let string1 = ""
+            let string2 = ""
+
+            for (let i in tokens) {
+                let token = tokens[i]
+
+                string1 += token.value + " "
+                string2 += (i == index ? "^" : " ").repeat(token.value.length) + " "
+            }
+
+            console.log(string1)
+            console.log(string2)
+
+            console.log()
+        } )
+    } else {
+        console.log(value)
+    }
+
+})()
