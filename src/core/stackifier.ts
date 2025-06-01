86fcd42e6f241af41e3fa35ae9cb5b971ca2a277
Felix Moses <felix@tacocat.com>
2025-06-01 18:10:43 +0200

    improved graph to js formating

--- src/core/stackifier.ts
+++ src/core/stackifier.ts
@@ -1,28 +1,26 @@
 import { Block, get_loops } from "@src/core/graph.ts";
 import assert from "node:assert";
 
-// Inspiration:
-// https://medium.com/leaningtech/solving-the-structured-control-flow-problem-once-and-for-all-5123117b1ee2
-
-function loop_graph(g: Block[]): Block[] {
-    for (const loop of get_loops(g)) {
+function loop_graph(graph: Block[]): Block[] {
+    for (const loop of get_loops(graph)) {
         // Loooop block!
         const loop_block = new Block()
-        loop_block.with("/* loop */")
         loop_block.data = { kind: "LOOP", loop: [] }
-        g.push(loop_block)
+        graph.push(loop_block)
 
         // Remove nodes from graph who are part of the loop 
-        g = g.filter(block => !loop.includes(block))
-    
+        graph = graph.filter(block => !loop.includes(block))
+
         // Figure out loop entry point.
         // TODO: make sure there is only one entry point to the loop
-        const entry = loop.find((b) => b.parents.some(parent => !loop.includes(parent)))
+        const entry = loop.find((b) => b.parents.some(p => !loop.includes(p)))
         if (!entry) throw new Error("Could not find entry point of loop!")
         loop_block.parents = entry.parents.filter(p => !loop.includes(p))
         entry.parents = []
 
         // Figure out possible targets of the loop.
+        // Note: one flaw here is that this add's a second imaginary parent,
+        // causing problems with the building of the code.
         for (const node of loop) {
             for (const child of node.children) {
                 if (!loop.includes(child)) {
@@ -33,18 +31,13 @@ function loop_graph(g: Block[]): Block[] {
 
         // Map entry node targets to the loop.
         for (const node of loop) {
-            node.children = node
-                .children
-                .map(e => e === entry ? loop_block : e)
+            node.children = node.children.map(e => e === entry ? loop_block : e)
         }
 
         // Map graph nodes targets to the loop, instead of the entry node
-        for (const node of g) {
-            node.children = node.children
-                .map(node => node === entry ? loop_block : node)
-
-            node.parents = node.parents
-                .map(node => node === entry ? loop_block : node)
+        for (const node of graph) {
+            node.children = node.children.map(n => n === entry ? loop_block : n)
+            node.parents = node.parents.map(n => n === entry ? loop_block : n)
         }
 
         // Sort the subgraph.
@@ -54,7 +47,7 @@ function loop_graph(g: Block[]): Block[] {
         }
     }
 
-    return g
+    return graph
 }
 
 function sort_graph(g: Block[]): Block[] {
@@ -83,43 +76,14 @@ const gotos = new Map<Block, string>
 function build_graph(g: Block[], level=0): string {
     // GOAL: Take a looped & sorted controle flow graph and turn it into code.
 
-    let src = ""
-
     // The scope level increases when we wrap the node, so this label
     // will be unique withing it's scope, but can be reused elsewhere.
     const label = `l${level}`
 
-    if (g.length > 1) {
-        // Is g[1] this only way to access g[0], and does it not branch?
-        const linar_from_parent = (
-            g[0].parents.length === 1
-            && g[0].parents[0] === g[1]
-            && g[1].children.length === 1
-        )
-
-        if (linar_from_parent) {
-            // If this node is linar from the parent, then no special action is
-            // needed to get to this node.
-            gotos.set(g[0], ``)
-
-            // We didn't use the label, so we don't need to increase our level.
-            src += build_graph(g.slice(1), level)
-        } else {
-            // We are wraping the upper blocks in a while loop, so to go to this
-            // node they simply need to break out it.
-            gotos.set(g[0], `break ${label}`)
-
-            // Wrap upper nodes in a while loop, and increase the scope level.
-            src += `${label}: do {`
-            src += build_graph(g.slice(1), level + 1)
-            src += `} while (0);`
-        }
-    }
-
     // Add the code
-    src += g[0].code.join(";") + ";"
+    let src = g[0].code.map(line => line + ";").join("")
 
-    // Add the target
+    // Add code to go the target node
     if (g[0].data?.kind === "LOOP") {
         // In a loop to target the entry point we need to continue, not break.
         gotos.set(g[0], `continue ${label}`)
@@ -132,11 +96,8 @@ function build_graph(g: Block[], level=0): string {
     } else if (g[0].data?.kind === "BRANCH") {
         assert(g[0].children.length === 2, "!2 children for branch block!")
 
-        src += `if (${g[0].data.cond}) {`
-        src += gotos.get(g[0].children[0])
-        src += `} else {`
+        src += `if (${g[0].data.cond}) {${gotos.get(g[0].children[0])}}`
         src += gotos.get(g[0].children[1])
-        src += `}`
     } else if (g[0].data?.kind === "RETURN") {
         src += `return ${g[0].data?.value};`
     } else if (g[0].children.length === 1) {
@@ -145,7 +106,31 @@ function build_graph(g: Block[], level=0): string {
         throw new Error("Invald block targets.")
     }
 
-    return src
+    // Add in next nodes
+    if (g.length > 1) {
+        if (g[0].parents.length === 1) {
+            // If we only have one parent then go to this code, we just need to
+            // paste it in directly. 
+            gotos.set(g[0], src)
+
+            // We didn't use the label, so we don't need to increase our level.
+            return build_graph(g.slice(1), level)
+        } else {
+            // We are wraping the upper blocks in a while loop, so to go to this
+            // node they need to break out it.
+            gotos.set(g[0], `break ${label}`)
+
+            // Wrap upper nodes in a while loop, and increase the scope level.
+            return [
+                `${label}: do {`,
+                build_graph(g.slice(1), level + 1),
+                `} while (0);`,
+                src,
+            ].join("")
+        }
+    } else {
+        return src
+    }
 }
 
 function get_graph_from_block(block: Block, graph=new Set<Block>()) {
